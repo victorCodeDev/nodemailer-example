d2b581082a67597d6690ce55f562b0457b43bdc3
victor.ojeda <ojedavictor80@gmail.com>
2021-03-23 00:27:04 -0300

    Creación de formulario de envio de mail

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const express = require(express);
+const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(), path.join(__dirname, 'public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-});
+app.listen(3000);
+console.log('Server on port 3000')
